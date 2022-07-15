6829b4f076ac2903276d3c659f875a73d57c5d90
Paul Bratslavsky <paulbratslavsky@Pauls-MBP.localdomain>
2022-07-15 13:15:15 -0500

    updated config folder

--- config/server.js
+++ config/server.js
@@ -1,12 +1,7 @@
 module.exports = ({ env }) => ({
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
-  },
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  cron: {
-    enabled: true
-  }
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
